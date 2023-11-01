eb45a06aeed6961653f1536494e1fc3ec00775cd
amit-shah-cb <143760822+amit-shah-cb@users.noreply.github.com>
2023-10-31 21:41:45 -0500

    add dummy webhook endpoint (#1)

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse,NextRequest } from 'next/server'
+
+// type commandOp = {
+//   command: string,
+//   targetUrl: string,
+//   userAddress: string
+// }
+
+export async function POST(req:NextRequest) {  
+  const body = await req.json()
+  console.log(JSON.stringify(body))
+  return NextResponse.json({status: 'ok'})
+} 
