9b661a6347758a896e5b64f72df94f5c9ec335a1
Amit Shah <amit.shah@coinbase.com>
2023-11-03 02:59:30 -0400

    latest migrations from remote

--- supabase/migrations/20231103065712_remote_schema.sql
+++ supabase/migrations/20231103065712_remote_schema.sql
@@ -24,10 +24,43 @@ CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
 
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE IF NOT EXISTS "public"."webhook_data" (
+    "transaction_hash" character varying NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "network_id" character varying NOT NULL,
+    "block_hash" "text",
+    "block_timestamp" character varying,
+    "event_type" character varying,
+    "from_address" character varying,
+    "to_address" character varying,
+    "value" character varying,
+    "contract_address" character varying,
+    "log_index" character varying DEFAULT ''::character varying,
+    "is_from_address_cbw" boolean DEFAULT false,
+    "is_to_address_cbw" boolean DEFAULT false
+);
+
+ALTER TABLE "public"."webhook_data" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."webhook_data"
+    ADD CONSTRAINT "webhook_data_pkey" PRIMARY KEY ("transaction_hash", "network_id");
+
+CREATE POLICY "Enable insert for authenticated users only" ON "public"."webhook_data" FOR INSERT TO "authenticated" WITH CHECK (true);
+
+ALTER TABLE "public"."webhook_data" ENABLE ROW LEVEL SECURITY;
+
 GRANT USAGE ON SCHEMA "public" TO "postgres";
 GRANT USAGE ON SCHEMA "public" TO "anon";
 GRANT USAGE ON SCHEMA "public" TO "authenticated";
 GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON TABLE "public"."webhook_data" TO "anon";
+GRANT ALL ON TABLE "public"."webhook_data" TO "authenticated";
+GRANT ALL ON TABLE "public"."webhook_data" TO "service_role";
 
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
