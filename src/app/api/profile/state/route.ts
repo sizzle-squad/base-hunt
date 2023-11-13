782223ed2ceaffe0edfb77316657e2330f30c40b
Amit Shah <amit.shah@coinbase.com>
2023-11-13 13:55:38 -0500

    add definition for join type

--- src/app/api/profile/state/route.ts
+++ src/app/api/profile/state/route.ts
@@ -1,15 +1,28 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 import '@/utils/helper';
-import { Badge } from '../../../../hooks/types';
+import { Badge, ChallengeTypeEnum } from '../../../../hooks/types';
 
 const prisma = new PrismaClient()
 
+type QueryData={
+    id:bigint;
+    name:string;    
+    image_url:string;
+    to_address:string;
+    type: ChallengeTypeEnum;
+    transaction_hash:string;
+    created_at:Date;
+    contract_address:string;
+    token_id:bigint;
+    cta_text:string;
+    cta_url:string;
+}
 export async function GET(req: Request) {
 
     const {searchParams} = new URL(req.url);
      //queryRaw is safe: https://www.prisma.io/docs/concepts/components/prisma-client/raw-database-access#queryraw
-     const data = await prisma.$queryRaw`select  b.name,b.image_url,b.contract_address,b.token_id,b.cta_url,b.cta_text,b.type,w.to_address,w.transaction_hash,w.created_at,w.event_type from badge_configuration as b LEFT join webhook_data as w
+     const data:QueryData[] = await prisma.$queryRaw`select  b.id,b.name,b.image_url,b.contract_address,b.token_id,b.cta_url,b.cta_text,b.type,w.to_address,w.transaction_hash,w.created_at,w.event_type from badge_configuration as b LEFT join webhook_data as w
      on LOWER(b.contract_address) = LOWER(w.contract_address) and b.token_id::bigint = substring(w.value,3)::bigint and LOWER(w.from_address) = LOWER(b.minter)
      and LOWER(w.to_address) = ${(searchParams.get("userAddress") as string).toLowerCase()} and b.game_id = ${( BigInt(searchParams.get("gameId") as string))}`
           
@@ -27,12 +40,13 @@ export async function GET(req: Request) {
 }
 
 
-function mapToBadge(b:any):Badge {
+function mapToBadge(b:QueryData):Badge {
+    console.log("queryData:",b);
     return {
-        id: b.id,
+        id: b.id.toString(),
         name: b.name,
         description: "",
-        imageUrl: b.image_url,
+        imageUrl: new URL(b.image_url) ,
         isCompleted: b.to_address != null,
         type: b.type,
         txHash: b.transaction_hash,
