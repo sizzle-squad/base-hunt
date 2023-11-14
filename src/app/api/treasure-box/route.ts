387053416cd0f67350cdb3e3ddf3f56334419c26
Amit Shah <amit.shah@coinbase.com>
2023-11-14 18:14:51 -0500

    update treasure-box get endpoint to join config and state table

--- src/app/api/treasure-box/route.ts
+++ src/app/api/treasure-box/route.ts
@@ -1,6 +1,8 @@
+import { TreasureBoxState } from '@/hooks/types';
 import { verifyTreasureBoxRequest } from '@/utils/verifyTreasureBoxRequest';
 import { PrismaClient } from '@prisma/client';
 import { NextResponse, type NextRequest } from 'next/server';
+import '@/utils/helper';
 
 const prisma = new PrismaClient();
 
@@ -15,6 +17,20 @@ export type TreasureBoxType = {
   user: UserPublicProfileType;
 };
 
+
+export type TreasureBoxStateType = 
+{
+  id: bigint,
+  created_at: Date,
+  total_hitpoints: bigint,
+  game_id: bigint,
+  name: string,
+  location: string,
+  current_hitpoints: bigint | null,
+  is_open: boolean | null
+}
+
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const gameId = searchParams.get('gameId');
@@ -23,24 +39,31 @@ export async function GET(request: NextRequest) {
     return new Response('Missing parameters: gameId', { status: 400 });
   }
 
-  const treasureBox = await prisma.treasure_box.findFirst({
-    where: {
-      game_id: BigInt(gameId as string),
-    },
-  });
-
-  if (!treasureBox) {
+  const treasureBox:TreasureBoxStateType[] = await prisma.$queryRaw`select tb.id,tb.created_at,tb.total_hitpoints,tb.game_id,tb.name,tb.location,tbs.current_hitpoints,tbs.is_open from treasure_box_configuration as tb LEFT JOIN treasure_box_state as tbs on tb.game_id = tbs.game_id where tb.game_id = ${BigInt(gameId)}`
+ 
+  if (!treasureBox || treasureBox.length === 0) {
     return new Response(`Treasure box not found with gameId: ${gameId}`, {
       status: 400,
     });
   }
-
-  return NextResponse.json({
-    hitPoints: treasureBox.hit_points,
-    isOpen: treasureBox.is_open,
-    location: treasureBox.location,
-  });
+  const tb = await mapTreasureBoxState(treasureBox[0]);
+  console.log(tb);
+  return NextResponse.json(tb);
 }
+
+export async function mapTreasureBoxState(treasureBox:TreasureBoxStateType): Promise<TreasureBoxState> {
+  return {
+    id: treasureBox.id as bigint,
+    createdAt: treasureBox.created_at,
+    gameId: treasureBox.game_id,
+    name: treasureBox.name,
+    location: treasureBox.location,
+    totalHitpoints: treasureBox.total_hitpoints,
+    currentHitpoints: treasureBox.current_hitpoints as bigint,
+    isOpen: treasureBox.is_open === true,
+  };
+}
+
 
 export async function POST(request: NextRequest) {
   const body: TreasureBoxType = await request.json();
