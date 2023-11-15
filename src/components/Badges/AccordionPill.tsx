c755473f0fc8506727e38aa1ada039190607cfa8
Travis Ueki <travueki@gmail.com>
2023-11-15 17:40:44 -0600

    accordion pill

---
+++ src/components/Badges/AccordionPill.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Box } from '@mui/material';
+
+function AccordionPill(props: any) {
+  return (
+    <Box
+      sx={{
+        color: 'var(--White, #fff)',
+        fontFeatureSettings: '"clig" 0, "liga" 0',
+        whiteSpace: 'nowrap',
+        borderRadius: '6.25rem', // 100px
+        backgroundColor: 'var(--Black, #151515)',
+        padding: '4px 8px',
+        font: '400 14px/17px "Coinbase Mono", -apple-system, Roboto, Helvetica, sans-serif',
+        '@media (max-width: 991px)': {
+          whiteSpace: 'initial',
+        },
+      }}
+    >
+      4/16
+    </Box>
+  );
+}
+
+export default AccordionPill;
