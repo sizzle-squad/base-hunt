bc90491ea7d96b3fe191b00e0ff1871d1b3927ab
Travis Ueki <travueki@gmail.com>
2023-11-15 13:19:00 -0600

    hero can now be ssr since navbar is dynamically imported

--- src/app/badges/page.tsx
+++ src/app/badges/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import Hero from '@/components/Badges/Hero';
 import BadgeContainer from '@/components/assets/BadgeContainer';
+import { useDrawer } from '@/context/DrawerContext';
 import { useBadges } from '@/hooks/useBadges';
 import { useClientCheck } from '@/hooks/useClientCheck';
-import { Box, Stack, Typography } from '@mui/material';
-import dynamic from 'next/dynamic';
+import { Box, Button, Drawer, Stack, Typography } from '@mui/material';
+import React from 'react';
 import { useMemo } from 'react';
 import { useAccount } from 'wagmi';
 
@@ -12,11 +14,8 @@ export default function Badges() {
   const isClient = useClientCheck();
   const { address, isConnected } = useAccount();
   const { useGetBadges } = useBadges({ address, isConnected });
-
-  // hydration issue without dynamic import
-  const Hero = dynamic(() => import('@/components/Badges/Hero'), {
-    ssr: false,
-  });
+  const [drawerOpen, setDrawerOpen] = React.useState(true);
+  const { drawerStates, toggleDrawer } = useDrawer();
 
   const { data: badges, isLoading, error } = useGetBadges();
 
@@ -75,10 +74,42 @@ export default function Badges() {
     }
   }, [isClient, badges, isLoading, error]);
 
+  type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      // onClick={toggleDrawer(anchor, false)}
+      // onKeyDown={toggleDrawer(anchor, false)}
+    >
+      {/* Add the content you want in the drawer here */}
+      <Typography variant="h6" sx={{ p: 2 }}>
+        Drawer Content Wallet
+      </Typography>
+    </Box>
+  );
+
   return (
     <Box>
       <Hero />
       <Box>{BadgesWrapper}</Box>
+      {(['bottom'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button
+            onClick={() => toggleDrawer('walletOperations', anchor, true)}
+          >
+            Wallet Operations {anchor}
+          </Button>
+          <Drawer
+            anchor={anchor}
+            open={drawerStates.walletOperations[anchor]}
+            onClose={() => toggleDrawer('walletOperations', anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
     </Box>
   );
 }
