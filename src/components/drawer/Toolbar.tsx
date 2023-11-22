6deb9654cd3028df624a6f4986fdcc1dcaaa47ee
Travis Ueki <travueki@gmail.com>
2023-11-21 19:54:07 -0600

    resuable toolbar for drawer

---
+++ src/components/drawer/Toolbar.tsx
@@ -0,0 +1,42 @@
+import {} from 'react';
+import { Stack, IconButton } from '@mui/material';
+import Text from '@/components/Text';
+
+type ToolBarProps = {
+  title: string;
+  onDismiss?: () => void;
+};
+
+const CloseIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="24" height="24" rx="12" fill="black" fill-opacity="0.2" />
+    <path d="M16 8L8 16M8 8L16 16" stroke="white" stroke-width="2" />
+  </svg>
+);
+
+const ToolBar: React.FC<ToolBarProps> = ({ title, onDismiss }) => {
+  return (
+    <Stack
+      flexDirection="row"
+      position="relative"
+      width="100%"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Stack flexDirection="row" justifyContent="space-between" width="100%">
+        <Text fontSize="20px">{title}</Text>
+      </Stack>
+      <IconButton onClick={onDismiss}>
+        <CloseIcon />
+      </IconButton>
+    </Stack>
+  );
+};
+
+export default ToolBar;
