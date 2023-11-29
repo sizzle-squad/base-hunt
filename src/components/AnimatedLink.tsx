e025d25005fac534a2a1d44859150934dc9f9ea3
Travis Ueki <travueki@gmail.com>
2023-11-29 17:55:48 -0600

    memo AnimatedLink

--- src/components/AnimatedLink.tsx
+++ src/components/AnimatedLink.tsx
@@ -1,18 +1,22 @@
 'use client';
 import useAnimatedRouter from '@/hooks/useAnimatedRouter';
 import Link from 'next/link';
-import React from 'react';
+import React, { memo } from 'react';
 
 type Props = {
   href: string;
   children: React.ReactNode;
 };
 
-export default function AnimatedLink({ href, children }: Props) {
+const AnimatedLink = ({ href, children }: Props) => {
   const { animatedRoute } = useAnimatedRouter();
   return (
     <Link href={href} onClick={() => animatedRoute(href)} passHref>
       {children}
     </Link>
   );
-}
+};
+
+export default memo(AnimatedLink);
+
+AnimatedLink.displayName = 'AnimatedLink';
