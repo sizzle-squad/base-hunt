0a22843c6998edcc8d0e10e0c7531e79730e1ffb
Taylor Caldwell <taylor.caldwell@coinbase.com>
2023-11-30 10:16:21 -0800

    Add icons

--- src/app/(drawerPages)/boosts/BoostsClient.tsx
+++ src/app/(drawerPages)/boosts/BoostsClient.tsx
@@ -5,7 +5,7 @@ import ListCard, { ListCardProps } from '@/components/ListCard';
 import ToolBar from '@/components/drawer/Toolbar';
 import DetailsPageNavbar from '@/components/navigation/DetailsPageNavbar';
 import { useDrawer } from '@/context/DrawerContext';
-import { Alert, Box, Button, NoSsr, Snackbar, Stack } from '@mui/material';
+import { Alert, Box, Button, NoSsr, Snackbar, Stack, SvgIcon } from '@mui/material';
 import { memo, useCallback, useMemo, useState, useEffect } from 'react';
 import { DrawerType } from '@/context/DrawerContext';
 import Text from '@/components/Text';
@@ -14,6 +14,17 @@ import { useBoosts } from '@/hooks/useBoosts';
 import { useClaimBoost } from '@/hooks/useClaimBoost';
 import { GAME_ID } from '@/constants/gameId';
 import { useAccount } from 'wagmi';
+import { WalletIcon, CoffeeIcon, BagIcon, GridIcon, CircleIcon, LinkIcon, UsersIcon } from '@/components/assets/icons/BoostIcon';
+
+const iconMapping = {
+  'WALLET': WalletIcon,
+  'COFFEE': CoffeeIcon,
+  'BAG': BagIcon,
+  'GRID': GridIcon,
+  'CIRCLE': CircleIcon,
+  'LINK': LinkIcon,
+  'USERS': UsersIcon
+};
 
 type ListCardPropsForBoosts = ListCardProps & {
   explanation?: string;
@@ -31,7 +42,7 @@ type ListCardPropsForBoosts = ListCardProps & {
 };
 
 const PageConsts = {
-  navTitle: 'Point Boost' as const,
+  navTitle: 'Point Boosts' as const,
   drawerTitle: 'Boosts' as const,
   drawerSubtitle: 'Completed 3 times' as const,
   drawerSubtitlePoints: 100000 as const,
@@ -59,21 +70,7 @@ export default function BoostsPageClient() {
     claimed: boost.claimed,
     claimable: !boost.claimed,
     startContent: (
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M9 11V6C9 4.34315 10.3431 3 12 3C13.6569 3 15 4.34315 15 6V10.9673M10.4 21H13.6C15.8402 21 16.9603 21 17.816 20.564C18.5686 20.1805 19.1805 19.5686 19.564 18.816C20 17.9603 20 16.8402 20 14.6V12.2C20 11.0799 20 10.5198 19.782 10.092C19.5903 9.71569 19.2843 9.40973 18.908 9.21799C18.4802 9 17.9201 9 16.8 9H7.2C6.0799 9 5.51984 9 5.09202 9.21799C4.71569 9.40973 4.40973 9.71569 4.21799 10.092C4 10.5198 4 11.0799 4 12.2V14.6C4 16.8402 4 17.9603 4.43597 18.816C4.81947 19.5686 5.43139 20.1805 6.18404 20.564C7.03968 21 8.15979 21 10.4 21Z"
-          stroke="#131A29"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
+      iconMapping[boost.icon]
     ),
     endContent: <Text>{boost.points.toString()} pts</Text>,
   }});
