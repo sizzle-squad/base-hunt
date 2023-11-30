6b6e3fd6870d8df2ac54f5743b1e5a3bed7096de
Taylor Caldwell <taylor.caldwell@coinbase.com>
2023-11-30 12:53:43 -0800

    Fix claim hook

--- src/hooks/useClaimBoost.ts
+++ src/hooks/useClaimBoost.ts
@@ -7,24 +7,19 @@ export type BoostsClaimData = {
     userAddress: `0x${string}` | undefined;
     boostId: string;
     contractAddress?: string;
-  };
+};
 
 export function useClaimBoost() {
-    const claimBoost = useMutation({
-      mutationFn: (data: BoostsClaimData) => { 
+    const claimBoost = useMutation(
+      (data: BoostsClaimData) => { 
         const { gameId, userAddress, boostId } = data;
 
         if (!userAddress || !gameId || !boostId) {
-            return new Response(
-              `Missing parameters: userAddress: ${userAddress}, gameId: ${gameId}, boostId: ${boostId}`,
-              {
-                status: 400,
-              }
-            );
-          }
-          
+          throw new Error(`Missing parameters: userAddress: ${userAddress}, gameId: ${gameId}, boostId: ${boostId}`);
+        }
+        
         return axios.post(routes.boosts.claim, data);
-      },
-    });
+      }
+    );
     return { claimBoost };
 }
