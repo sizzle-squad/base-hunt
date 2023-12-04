4d3f9deaa3b09792d8f8b03de09812c6ea1ad56b
Amit Shah <amit.shah@coinbase.com>
2023-12-04 11:59:14 -0500

    integrate iyk tap into tap-reveal workflow

--- src/utils/iyk.ts
+++ src/utils/iyk.ts
@@ -22,33 +22,38 @@ export interface PoapEvent {
 }
 
 export async function getTapRef(iykRef: string): Promise<TapRef | null> {
-  const data = await axios.get<TapRef>(`https://api.iyk.app/refs/${iykRef}`, {
-    headers: {
-      'x-iyk-api-key': process.env.IYK_API_KEY as string,
-    },
-  });
-  if (data.status !== 200) {
-    return null;
-  }
-  /*
-    {
-        "uid": "1111111111144444",
-        "isValidRef": false,
-        "linkedToken": {
-            "contractAddress": "0x067f20be8e84c8fed733578e7249ea911111111",
-            "chainId": 1,
-            "tokenId": "1",
-            "otp": "unique-21char-Nano_Id"
-        },
-        "poapEvents": [
-            {
-            "id": 1,
-            "poapEventId": 123456,
-            "otp": "unique-21char-Nano_Id",
-            "status": "active"
-            }
-        ]
+  try {
+    const data = await axios.get<TapRef>(`https://api.iyk.app/refs/${iykRef}`, {
+      headers: {
+        'x-iyk-api-key': process.env.IYK_API_KEY as string,
+      },
+    });
+    if (data.status !== 200) {
+      return null;
     }
-    */
-  return data.data;
+    /*
+      {
+          "uid": "1111111111144444",
+          "isValidRef": false,
+          "linkedToken": {
+              "contractAddress": "0x067f20be8e84c8fed733578e7249ea911111111",
+              "chainId": 1,
+              "tokenId": "1",
+              "otp": "unique-21char-Nano_Id"
+          },
+          "poapEvents": [
+              {
+              "id": 1,
+              "poapEventId": 123456,
+              "otp": "unique-21char-Nano_Id",
+              "status": "active"
+              }
+          ]
+      }
+      */
+    return data.data;
+  } catch (e) {
+    console.error(e);
+  }
+  return null;
 }
