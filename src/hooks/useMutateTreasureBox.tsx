2ee249e96d4a8271475b5b41895376e7625595b0
Travis Ueki <travueki@gmail.com>
2023-12-04 17:46:40 -0600

    Update treasurebox mutation to invalidate treasurebox queries on successful mutation

--- src/hooks/useMutateTreasureBox.tsx
+++ src/hooks/useMutateTreasureBox.tsx
@@ -1,16 +1,20 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { TreasureBoxType } from '@/app/api/treasure-box/route';
 import { routes } from '@/constants/routes';
 import axios from 'axios';
 import { verifyTreasureBoxRequest } from '@/utils/verifyTreasureBoxRequest';
 
-export function useMutateTreasureBox() {
+export function useMutateTreasureBox({ gameId }: { gameId: string }) {
+  const queryClient = useQueryClient();
   const attackBox = useMutation({
     mutationFn: (data: TreasureBoxType) => {
       // make sure data is correct
 
       verifyTreasureBoxRequest(data);
       return axios.post(routes.treasureBox.default, data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['treasure-box', gameId]);
     },
   });
 
