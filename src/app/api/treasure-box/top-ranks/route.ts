3e09ac04a544e3b99d07b8091129c501bb05e40d
Amit Shah <amit.shah@coinbase.com>
2023-12-04 00:00:07 -0500

    update top-rank query to also order by updated_at asc

--- src/app/api/treasure-box/top-ranks/route.ts
+++ src/app/api/treasure-box/top-ranks/route.ts
@@ -24,6 +24,7 @@ export async function GET(request: NextRequest) {
     .select('*')
     .eq('game_id', BigInt(gameId))
     .order('total_hitpoints', { ascending: false })
+    .order('updated_at', { ascending: true })
     .limit(10);
 
   if (error) {
