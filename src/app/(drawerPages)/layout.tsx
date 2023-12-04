ebd43d5aa071441cb1c1d767aa46f2aece7ad6ab
Travis Ueki <travueki@gmail.com>
2023-12-04 11:31:38 -0600

    Travis/score page (#125)
    * update layout to remove global max width
    
    * add max width to drawerpages group
    
    * Countdown component
    
    * add accurate-interval
    
    * add reveal countdown page and hooks

--- src/app/(drawerPages)/layout.tsx
+++ src/app/(drawerPages)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/navigation/footer';
 import { DrawerProvider } from '@/context/DrawerContext';
+import { Box } from '@mui/material';
 
 export default function ArtRevealLayout({
   children,
@@ -8,8 +9,10 @@ export default function ArtRevealLayout({
 }) {
   return (
     <DrawerProvider>
-      {children}
-      <Footer />
+      <Box maxWidth={390} sx={{ margin: '0 auto' }}>
+        {children}
+        <Footer />
+      </Box>
     </DrawerProvider>
   );
 }
