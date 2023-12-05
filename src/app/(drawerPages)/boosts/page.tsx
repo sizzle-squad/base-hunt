a41560dd1a18a6caa5378330200e23885291f26f
siwan-cb <136514599+siwan-cb@users.noreply.github.com>
2023-12-05 10:01:16 -0800

    remove extra margin (#153)

--- src/app/(drawerPages)/boosts/page.tsx
+++ src/app/(drawerPages)/boosts/page.tsx
@@ -3,7 +3,7 @@ import BoostsPageClient from './BoostsClient';
 
 export default function Page() {
   return (
-    <Stack gap="24px" mt="24px" paddingY="24px" paddingX="20px">
+    <Stack gap="24px" paddingY="24px" paddingX="20px">
       <BoostsPageClient />
     </Stack>
   );
--- src/app/(drawerPages)/leaderboard/page.tsx
+++ src/app/(drawerPages)/leaderboard/page.tsx
@@ -3,7 +3,7 @@ import LeaderboardClient from './LeaderboardClient';
 
 export default function Page() {
   return (
-    <Stack mt="24px" paddingY="24px" paddingX="20px">
+    <Stack paddingY="24px" paddingX="20px">
       <LeaderboardClient />
     </Stack>
   );
