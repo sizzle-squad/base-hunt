28c11362b5a973dc9492dc9b830a696e45aae69f
Travis Ueki <travueki@gmail.com>
2023-12-04 18:45:08 -0600

    add date-fns helpers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { isAfter, isBefore } from 'date-fns';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const startDate = new Date(process.env.START_DATE as string);
   const endDate = new Date(process.env.END_DATE as string);
   const now = new Date();
-  const killSwitch = process.env.KILL_SWITCH ?? false;
+  const killSwitch = Boolean(process.env.KILL_SWITCH);
 
-  if (killSwitch || now < startDate || now > endDate) {
+  if (killSwitch || isBefore(now, startDate) || isAfter(now, endDate)) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 }
