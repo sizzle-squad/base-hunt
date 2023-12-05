63ac65496cdcf3cb1e485c0ccff12244361f4c28
Travis Ueki <travueki@gmail.com>
2023-12-04 18:42:19 -0600

    use env for all

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,12 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const startDate = new Date('2021-09-01T00:00:00.000Z');
-  const endDate = new Date('2022-09-30T23:59:59.999Z');
+  const startDate = new Date(process.env.START_DATE as string);
+  const endDate = new Date(process.env.END_DATE as string);
   const now = new Date();
   const killSwitch = process.env.KILL_SWITCH ?? false;
 
-  if (now < startDate || now > endDate || killSwitch) {
+  if (killSwitch || now < startDate || now > endDate) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 }
