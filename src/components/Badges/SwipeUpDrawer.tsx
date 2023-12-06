851852e6b1ca1c333799043711ef1791e81cec09
Travis Ueki <travueki@gmail.com>
2023-12-06 16:48:54 -0600

    Travis/better map fix (#186)
    * fix swipeable drawer for maps
    
    * update toggle sideeffect

--- src/components/Badges/SwipeUpDrawer.tsx
+++ src/components/Badges/SwipeUpDrawer.tsx
@@ -96,6 +96,15 @@ function SwipeUpDrawer({
     setIsMapOpen(true);
   }, []);
 
+  const handleToggleDrawer = useCallback(() => {
+    if (!isMapOpen) {
+      toggleDrawer(type, anchor, !drawerStates.badgeActions[anchor]);
+    } else {
+      setIsMapOpen(false);
+      toggleDrawer(type, anchor, !drawerStates.badgeActions[anchor]);
+    }
+  }, [isMapOpen]);
+
   const ctaButton = useMemo(() => {
     if (owned) return null;
 
@@ -184,7 +193,7 @@ function SwipeUpDrawer({
           },
         }}
       />
-      <Box onClick={() => toggleDrawer(type, anchor, true)}>
+      <Box onClick={handleToggleDrawer}>
         <SwipeableDrawer
           allowSwipeInChildren
           anchor={anchor}
@@ -193,6 +202,7 @@ function SwipeUpDrawer({
           onOpen={() => toggleDrawer(type, anchor, true)}
           swipeAreaWidth={100}
           disableSwipeToOpen={false}
+          keepMounted
           ModalProps={{
             keepMounted: true,
           }}
