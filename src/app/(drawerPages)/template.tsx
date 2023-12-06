77060f0aef429e165327e35ccd6ce2d6b7576004
Travis Ueki <travueki@gmail.com>
2023-12-06 00:03:51 -0600

    hide footer on leaderboard

--- src/app/(drawerPages)/template.tsx
+++ src/app/(drawerPages)/template.tsx
@@ -5,7 +5,10 @@ import { useMemo } from 'react';
 
 export default function Template(props: any) {
   const pathname = usePathname();
-  const noShowRoutes = useMemo(() => ['/levels', '/boosts'], []);
+  const noShowRoutes = useMemo(
+    () => ['/levels', '/boosts', '/leaderboard'],
+    []
+  );
   const showFooter = useMemo(
     () => !noShowRoutes.includes(pathname),
     [pathname, noShowRoutes]
