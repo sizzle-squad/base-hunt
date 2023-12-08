c62af3fc9f8a91f217d653d19e512c63c6a85135
Travis Ueki <travueki@gmail.com>
2023-12-08 14:35:12 -0600

    add host

--- src/app/(drawerPages)/badges/[id]/badgeClient.tsx
+++ src/app/(drawerPages)/badges/[id]/badgeClient.tsx
@@ -59,7 +59,12 @@ export default function BadgeDetails({ params }: Props) {
 
   const twitterShareUrl = useMemo(() => {
     const text = `Check out this badge: ${currentBadge?.name}`;
-    const url = `https://basehunt.xyz/badge/${currentBadge?.id}`; // Replace with actual badge URL
+    const host =
+      process.env.NODE_ENV === 'production'
+        ? 'https://basehunt.xyz'
+        : window.location.hostname;
+
+    const url = `${host}/${currentBadge?.id}`; // Replace with actual badge URL
     return `https://twitter.com/intent/tweet?text=${encodeURIComponent(
       text
     )}&url=${encodeURIComponent(url)}`;
