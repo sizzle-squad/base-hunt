f79f7e587604251831609abc334b95cf3b4124e0
Travis Ueki <travueki@gmail.com>
2023-12-08 00:56:22 -0600

    add badge/id to hide footer

--- src/app/(drawerPages)/template.tsx
+++ src/app/(drawerPages)/template.tsx
@@ -7,13 +7,17 @@ import { ReactNode, useMemo } from 'react';
 export default function Template({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const noShowRoutes = useMemo(
-    () => ['/levels', '/boosts', '/leaderboard'],
+    () => [
+      /^\/levels$/,
+      /^\/boosts$/,
+      /^\/leaderboard$/,
+      /^\/badges\/.+$/, // This regex will match any route starting with /badges/ followed by any characters
+    ],
     []
   );
-  const showFooter = useMemo(
-    () => !noShowRoutes.includes(pathname),
-    [pathname, noShowRoutes]
-  );
+  const showFooter = useMemo(() => {
+    return !noShowRoutes.some((regex) => regex.test(pathname));
+  }, [pathname, noShowRoutes]);
 
   return (
     <Stack justifyContent="space-between" width="100vw" height={['100vh']}>
