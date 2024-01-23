82f09dd1c8fda45bdfbc0d72855da07cc4649e78
Si Wan Kim <siwan.kim@coinbase.com>
2024-01-23 15:07:32 -0800

    Revert "filter by difficulty types (#231)"
    This reverts commit cb70a5d5ac5fff83d9c5aa65a70560746681189a.

---
+++ src/hooks/useBoosts.ts
@@ -0,0 +1,42 @@
+import { routes } from '@/constants/routes';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { Boost } from './types';
+import { useMemo } from 'react';
+
+type Props = {
+  userAddress: `0x${string}` | undefined;
+  gameId: string;
+};
+
+export function useBoosts({ userAddress, gameId }: Props) {
+  const { data, isLoading, error } = useQuery<Boost[]>(
+    ['boosts', userAddress, gameId],
+    async () => {
+      const boosts = await axios({
+        method: 'GET',
+        url: `${routes.boosts.default}`,
+        params: {
+          userAddress: userAddress,
+          gameId: gameId,
+        },
+      });
+      return boosts.data;
+    },
+    {
+      enabled: !!userAddress && gameId !== undefined,
+      onError: (error) => {
+        console.error(error);
+      },
+    }
+  );
+
+  return useMemo(
+    () => ({
+      data,
+      isLoading,
+      error,
+    }),
+    [data, error, isLoading]
+  );
+}
