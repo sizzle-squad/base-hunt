afcbddb8f6500b76dd5af7aee8c15d2fa0b390ed
Amit Shah <amit.shah@coinbase.com>
2024-01-24 21:37:30 -0500

    handle provider selection on auto-claim

--- src/utils/claims/selectors.ts
+++ src/utils/claims/selectors.ts
@@ -5,6 +5,7 @@ import { checkMint } from './mintCheck';
 import { checkFunctionExecution } from './transactionCheck';
 import { checkTrivia } from './triviaCheck';
 import { checkTxCountBatch } from './txHistoryCheck';
+import { Database } from '../database.types';
 
 export const CheckFunctions: {
   [key in CheckFunctionType]: (...args: any[]) => Promise<boolean>;
@@ -41,6 +42,29 @@ export const MapChallengeTypeUserAddress: {
   [CheckFunctionType.checkBalance]: function (
     w: WebhookData
   ): string | undefined {
+    throw new Error('Function not implemented.');
+  },
+};
+
+export const ScoreFunctions: {
+  [key in CheckFunctionType]: (...args: any[]) => number;
+} = {
+  [CheckFunctionType.checkMint]: function (w: any): number {
+    throw new Error('Function not implemented.');
+  },
+  [CheckFunctionType.checkTrivia]: function (w: any): number {
+    throw new Error('Function not implemented.');
+  },
+  [CheckFunctionType.checkFunctionExecution]: function (w: any): number {
+    throw new Error('Function not implemented.');
+  },
+  [CheckFunctionType.checkBalance]: function (w: any): number {
+    throw new Error('Function not implemented.');
+  },
+  [CheckFunctionType.checkTokenIdBalance]: function (w: any): number {
+    throw new Error('Function not implemented.');
+  },
+  [CheckFunctionType.getTxCountBatch]: function (w: any): number {
     throw new Error('Function not implemented.');
   },
 };
