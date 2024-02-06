c9ff16ac804ab0cd7d1c1b86fd99de73e1a4f779
Amit Shah <amit.shah@coinbase.com>
2024-02-05 23:10:22 -0500

    add point distribution handler to be run once daily

--- src/utils/inngest/functions.ts
+++ src/utils/inngest/functions.ts
@@ -1,12 +1,14 @@
-import axios from 'axios';
 import { inngest } from './client';
 import { Database } from '../database.types';
-import { createClient } from '@supabase/supabase-js';
-import { getTxCountBatch } from '../claims/txHistoryCheck';
-import { Networks } from '../database.enums';
+import { PostgrestError, createClient } from '@supabase/supabase-js';
+import {
+  ChallengeStatus,
+  ChallengeType,
+  CheckFunctionType,
+  Networks,
+} from '../database.enums';
 import { providers } from '../ethereum';
 import { ethers } from 'ethers';
-import { get } from 'http';
 
 const supabase = createClient<Database>(
   process.env.SUPABASE_URL as string,
@@ -144,3 +146,100 @@ function chunkArray<T>(array: T[], size: number): T[][] {
     array.slice(i * size, i * size + size)
   );
 }
+
+export const userPointDistribute = inngest.createFunction(
+  { id: 'user-point-distribute' },
+  { event: 'events/user-point-distribute' },
+  async ({ event, step }) => {
+    // Get current date in MST
+    let now = new Date();
+    let mstNow = now.toLocaleString('en-US', { timeZone: 'America/Denver' });
+
+    // Get start of day in MST
+    let start = new Date(mstNow);
+    start.setHours(0, 0, 0, 0);
+    let mstStart = start.toLocaleString('en-US', {
+      timeZone: 'America/Denver',
+    });
+
+    // Get end of day in MST
+    let end = new Date(mstNow);
+    end.setHours(23, 59, 59, 999);
+    let mstEnd = end.toLocaleString('en-US', { timeZone: 'America/Denver' });
+
+    const challengeData = await supabase
+      .from('challenge_configuration')
+      .select()
+      .eq('type', ChallengeType.GUILD)
+      .eq('function_type', CheckFunctionType.checkTxCountBatch)
+
+      .single();
+
+    if (challengeData.error) {
+      console.error(challengeData.error);
+      return;
+    }
+
+    const challenge = challengeData.data;
+
+    const guildId: string | null = await step.run(
+      'fetch-guild-with-highest-score',
+      async () => {
+        const { data, error } = await supabase
+          .from('guild_score')
+          .select('guild_id')
+          .eq('game_id', event.data.gameId as number)
+          .order('score', { ascending: false })
+          .limit(1);
+        if (error) {
+          console.error(error);
+          return null;
+        }
+
+        return data[0].guild_id;
+      }
+    );
+
+    if (guildId === null) {
+      console.error('No guild found with highest score');
+      return;
+    }
+
+    const users = await step.run('fetch-guild-users', async () => {
+      const { data, error } = await supabase
+        .from('guild_member_configuration')
+        .select('user_address')
+        .eq('game_id', event.data.gameId as number)
+        .eq('guild_id', guildId as string);
+      if (error) {
+        console.error(error);
+        return [];
+      }
+      return data;
+    });
+
+    const rows = [];
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      const row = await step.run('update-challenge', async () => {
+        const d = {
+          user_address: user.user_address,
+          challenge_id: challenge.id,
+          status: ChallengeStatus.COMPLETE,
+          points: challenge.points as number,
+        };
+        // TODO: uncomment this
+        // await supabase
+        //   .from('user_challenge_status')
+        //   .insert(d)
+        //   .select();
+        return d;
+      });
+
+      await step.sleep('wait-a-moment', '10 milliseconds');
+      rows.push(row);
+    }
+
+    return { data: rows };
+  }
+);
