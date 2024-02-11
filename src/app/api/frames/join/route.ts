52859c2ccea00ea7f35affe2b22a9fb143a1bea5
Amit Shah <amit.shah@coinbase.com>
2024-02-11 00:23:38 -0500

    create initial frame entry page

--- src/app/api/frames/join/route.ts
+++ src/app/api/frames/join/route.ts
@@ -12,81 +12,26 @@ const supabase = createClient(
 
 //const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY as string);
 
-export async function GET(req: NextRequest) {
-  const searchParams = req.nextUrl.searchParams;
-  const userAddress = searchParams.get('userAddress') as string;
-  const gameId = toBigInt(searchParams.get('gameId') as string);
-  if (!userAddress || gameId === null) {
-    return new Response(
-      `Missing parameters: userAddress: ${userAddress}, gameId: ${gameId}`,
-      {
-        status: 400,
-      }
-    );
-  }
-
-  //get user guild
-  const userGuildData = await supabase
-    .from('guild_member_configuration')
-    .select('*')
-    .eq('game_id', gameId)
-    .eq('user_address', userAddress)
-    .maybeSingle();
-  if (userGuildData.error) {
-    console.error(userGuildData.error);
-    return new Response(
-      `Error getting guild membership: userAddress: ${userAddress}, gameId: ${gameId}`,
-      {
-        status: 400,
-      }
-    );
-  }
-
-  const guildData = await supabase
-    .from('guild_configuration')
-    .select('*')
-    .eq('game_id', gameId)
-    .eq('guild_id', userGuildData.data.guild_id)
-    .single();
-  if (guildData.error) {
-    console.error(guildData.error);
-    return new Response(
-      `Error getting guild: guild_id: ${userGuildData.data.guild_id}, gameId: ${gameId}`,
-      {
-        status: 400,
-      }
-    );
-  }
-  let guild = guildData.data;
-
-  return new NextResponse(`<!DOCTYPE html><html><head>
-  <meta name="fc:frame" content="vNext" />
-  <meta name="fc:frame:image" content="https://i.ibb.co/tzy655S/guild-base-glitch.png"/>
-  <meta name="fc:frame:button:1" content="Join ${guild.name}" />
-  <meta name="fc:frame:button:1:action" content="post" />
-  <meta name="fc:frame:button:1:action:target" content="/api/frames/join?userAddress=${userAddress}&gameId=${gameId}&guildId=${guild.guild_id}" />
- /head></html>`);
-}
-
 export async function POST(req: NextRequest) {
-  const userAddress = req.nextUrl.searchParams.get('userAddress');
-  const gameId = req.nextUrl.searchParams.get('gameId');
-  const guildId = req.nextUrl.searchParams.get('guildId');
-
+  // const userAddress = req.nextUrl.searchParams.get('userAddress');
+  // const gameId = req.nextUrl.searchParams.get('gameId');
+  // const guildId = req.nextUrl.searchParams.get('guildId');
+  console.log('searchParams:', req.nextUrl.searchParams);
   const body = await req.json();
-
+  console.log('body:', body);
   const fm = await getFrameMessage(body, {
     neynarApiKey: 'NEYNAR_API_DOCS',
     castReactionContext: true,
     followContext: true,
   });
 
-  console.log(fm);
-  return new NextResponse(`<!DOCTYPE html><html><head>
+  console.log('fm:', JSON.stringify(fm));
+  return new NextResponse(`
+  <!DOCTYPE html><html><head>
   <meta name="fc:frame" content="vNext" />
   <meta name="fc:frame:image" content="https://i.ibb.co/tzy655S/guild-base-glitch.png"/>
   <meta name="fc:frame:button:1" content="Happy Hunting" />
   <meta name="fc:frame:button:1:action" content="link" />
   <meta name="fc:frame:button:1:action:target" content="https://go.cb-w.com/dapp?cb_url=https%3A%2F%2Fbase-hunt-eth-denver-2024.vercel.app%2F" />
- /head></html>`);
+  </head></html>`);
 }
---
+++ src/app/frames/join/page.tsx
@@ -0,0 +1,86 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata, ResolvingMetadata } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
+import { NeynarAPIClient } from '@neynar/nodejs-sdk';
+import { createClient } from '@supabase/supabase-js';
+import Image from 'next/image';
+import { toBigInt } from '@/utils/toBigInt';
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string };
+};
+
+const supabase = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_ANON_KEY as string
+);
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  console.log('generating metadata:', searchParams);
+  const userAddress = searchParams['userAddress'] as string;
+  const gameId = toBigInt(searchParams['gameId'] as string);
+  if (!userAddress || gameId === null) {
+    console.log('no user address or gameId');
+  }
+  console.log('generating metadata:', userAddress, gameId);
+  //get user guild
+  const userGuildData = await supabase
+    .from('guild_member_configuration')
+    .select('*')
+    .eq('game_id', gameId)
+    .eq('user_address', userAddress)
+    .maybeSingle();
+  if (userGuildData.error) {
+    console.error(userGuildData.error);
+  }
+
+  const guildData = await supabase
+    .from('guild_configuration')
+    .select('*')
+    .eq('game_id', gameId)
+    .eq('guild_id', userGuildData.data.guild_id)
+    .single();
+  if (guildData.error) {
+    console.error(guildData.error);
+  }
+  let guild = guildData.data;
+
+  console.log(guild);
+  const frameMetadata = await getFrameMetadata({
+    buttons: [
+      {
+        label: `Join ${guild.name}`,
+      },
+    ],
+    image: 'https://i.ibb.co/tzy655S/guild-base-glitch.png',
+    postUrl: `/api/frames/join?userAddress=${userAddress}&gameId=${gameId}&guildId=${guild.guild_id}`,
+  });
+
+  const _metadata: Metadata = {
+    title: 'Base Hunt Guilds',
+    description: 'Join a guild and start hunting!',
+    openGraph: {
+      title: 'Base Hunt Guilds',
+      description: 'Join a guild and start hunting!',
+      images: ['https://i.ibb.co/tzy655S/guild-base-glitch.png'],
+    },
+    other: {
+      ...frameMetadata,
+    },
+  };
+  return _metadata;
+}
+
+export default function Page({ params, searchParams }: Props) {
+  console.log(searchParams);
+
+  return (
+    <>
+      <h1>Welcome to Base Hunt</h1>
+    </>
+  );
+}
