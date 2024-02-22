28a3ac6caa0a6cb377dcdadc85fc5c6f8234605b
Si Wan Kim <siwan.kim@coinbase.com>
2024-02-22 14:59:03 -0800

    wrap

--- src/app/(drawerPages)/(detailPages)/leaderboard/guild/GuildCardList.tsx
+++ src/app/(drawerPages)/(detailPages)/leaderboard/guild/GuildCardList.tsx
@@ -46,11 +46,11 @@ export function GuildCardList({ guilds }: { guilds: Guild[] }) {
       gap={1}
     >
       <Box pt={2}>
-        <Text textAlign="center">
-          Join a guild, earn more points and have more fun! If your guild has
-          the most points at the end of each day (5 P.M MST), you’ll earn an
-          extra 100 points for that day. You’ll have 7 opportunities to win the
-          daily guild challenge. Choose wisely!
+        <Text textAlign="center" whiteSpace="pre-wrap">
+          Join a guild, earn more points and have more fun! <br />
+          If your guild has the most points at the end of each day (5 PM MST),
+          you’ll earn an extra 100 points for that day. You’ll have 7
+          opportunities to win the daily guild challenge. Choose wisely!
         </Text>
       </Box>
       <Grid
