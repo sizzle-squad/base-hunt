1d2ff1c40e0db6df63d3b45a9064bdc02c3dbdc8
akram eldamaty <akram.eldamaty@coinbase.com>
2024-06-05 11:48:05 -0400

    add challenge completion endpoint

---
+++ src/app/api/challenges/completion/route.ts
@@ -0,0 +1,129 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from 'next/server';
+import { ChallengeCompletionState } from '@/hooks/types';
+import { Database } from '@/utils/database.types';
+import { toBigInt } from '@/utils/toBigInt';
+import { ChallengeStatus } from '@/utils/database.enums';
+
+const supabase = createClient<Database>(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_KEY as string
+);
+
+interface ChallengeConfigurationType {
+  id: number;
+  challenge_id: string;
+}
+
+interface UserChallengeStatusType {
+  id: number;
+  user_address: string;
+  challenge_id: number;
+  status: string;
+}
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const gameId = toBigInt(searchParams.get('gameId') as string);
+  const userAddress = searchParams.get('userAddress') as string;
+  const challengeIds = searchParams.getAll('challengeIds').map(String);
+
+  if (!gameId || !userAddress || challengeIds.length === 0) {
+    return new Response(
+      'Missing parameters: gameId, userAddress, or challengeIds',
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    // Fetch challenge configuration based on challengeIds
+    const { data: challengeConfigurations, error: configError } = await supabase
+      .from('challenge_configuration')
+      .select('*')
+      .in('challenge_id', challengeIds);
+
+    if (configError) {
+      console.error(configError);
+      return new Response('Error querying challenge configurations', {
+        status: 500,
+      });
+    }
+
+    if (!challengeConfigurations || challengeConfigurations.length === 0) {
+      return NextResponse.json(
+        {
+          error: `No challenge configurations found for the provided challengeIds`,
+        },
+        { status: 400 }
+      );
+    }
+
+    // Extract challenge IDs from fetched challenge configurations
+    const challengeIdsFromConfigurations = challengeConfigurations.map(
+      (config) => config.id
+    );
+
+    // Fetch user challenge status based on userAddress and the challenge IDs from configurations
+    const { data: userChallengeStatusData, error: statusError } = await supabase
+      .from('user_challenge_status')
+      .select('*')
+      .in('challenge_id', challengeIdsFromConfigurations)
+      .eq('user_address', userAddress.toLowerCase());
+
+    if (statusError) {
+      console.error(statusError);
+      return new Response('Error querying user challenge status', {
+        status: 500,
+      });
+    }
+
+    if (!userChallengeStatusData || userChallengeStatusData.length === 0) {
+      return NextResponse.json(
+        {
+          error: `No available metrics found for gameId: ${gameId} and userAddress: ${userAddress}`,
+        },
+        { status: 400 }
+      );
+    }
+
+    let challengeConfigurationsMap = new Map<
+      number,
+      ChallengeConfigurationType
+    >();
+    challengeConfigurations.forEach((config) => {
+      challengeConfigurationsMap.set(config.id, config);
+    });
+
+    return NextResponse.json(
+      mapToChallengeCompletionState(
+        userChallengeStatusData,
+        challengeConfigurationsMap
+      )
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        error: `An error occurred while fetching metrics for gameId: ${gameId} and userAddress: ${userAddress}`,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+function mapToChallengeCompletionState(
+  userChallengeStatusData: UserChallengeStatusType[],
+  challengeConfigurationsMap: Map<number, ChallengeConfigurationType>
+): ChallengeCompletionState[] {
+  return userChallengeStatusData.map((userChallengeStatus) => {
+    return {
+      id: userChallengeStatus.challenge_id,
+      challengeId:
+        challengeConfigurationsMap.get(userChallengeStatus.challenge_id)
+          ?.challenge_id ?? '',
+      hasCompleted: userChallengeStatus.status === ChallengeStatus.COMPLETE,
+    };
+  });
+}
