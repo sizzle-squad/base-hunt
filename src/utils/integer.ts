06a391443d229447f881dba61be23c074511cc44
Si Wan Kim <siwan.kim@coinbase.com>
2024-07-17 10:26:10 -0700

    0 is not a valid token amount

--- src/utils/integer.ts
+++ src/utils/integer.ts
@@ -2,5 +2,5 @@ export function isStringAnInteger(str: string): boolean {
   const num = parseInt(str, 10);
 
   // Check if the parsed number is a valid number and if it matches the original string
-  return !isNaN(num) && num.toString() === str;
+  return !isNaN(num) && num.toString() === str && num >= 0;
 }
