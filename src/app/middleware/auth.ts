4de70f6be7c619dfab8aba53750828c2ea10f045
akram eldamaty <akram.eldamaty@coinbase.com>
2024-07-24 13:15:09 -0400

    add GET endpoint

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function authenticate(req: NextRequest, next: () => Promise<NextResponse>) {
+export async function authenticate(
+  req: NextRequest,
+  next: () => Promise<NextResponse>
+) {
   const apiKey = req.headers.get('x-api-key');
 
   if (!apiKey || apiKey !== process.env.BH_PRIVATE_ENDPOINTS_API_KEY) {
