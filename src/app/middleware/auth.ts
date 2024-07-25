4094aee15490999201456e18db482d2f32ee3cd4
akram eldamaty <akram.eldamaty@coinbase.com>
2024-07-25 11:17:30 -0400

    log key

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -5,6 +5,9 @@ export async function authenticate(
   next: () => Promise<NextResponse>
 ) {
   const apiKey = req.headers.get('x-api-key');
+  console.warn(
+    `Onboarding Leaderboard API Key: ${process.env.BH_PRIVATE_ENDPOINTS_API_KEY}, apiKey: ${apiKey}`
+  );
 
   if (!apiKey || apiKey !== process.env.BH_PRIVATE_ENDPOINTS_API_KEY) {
     return new Response('Unauthorized', { status: 401 });
