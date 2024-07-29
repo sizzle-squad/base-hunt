4d06bbf95a0bbc70992893eb20888bd1063502e2
Daivic Vora <daivic.vora@coinbase.com>
2024-07-29 09:53:02 -0700

    payout check only if USDC is won

--- src/app/api/spin-the-wheel/execute/route.ts
+++ src/app/api/spin-the-wheel/execute/route.ts
@@ -60,16 +60,6 @@ export async function POST(request: NextRequest) {
   }
 
   try {
-    const payouts = (await supabase.rpc('get_recent_payouts'))?.data;
-    if (payouts && payouts >= 2000) {
-      console.error('Too many payouts');
-      return new Response(
-        `Too many payouts have been made recently, please try again later`,
-        {
-          status: 400,
-        }
-      );
-    }
     const spinDataRes = await supabase.rpc('getspindata', {
       _game_id: gameId,
       _user_address: userAddress.toLowerCase(),
@@ -112,6 +102,18 @@ export async function POST(request: NextRequest) {
     }
 
     const generatedSpin = getRandomOutcome(currentlyEnabledSpins);
+    if (generatedSpin.type == SpinOptionTypeEnum.USDC) {
+      const payouts = (await supabase.rpc('get_recent_payouts'))?.data;
+      if (payouts && payouts >= 2000) {
+        console.error('Too many payouts');
+        return new Response(
+          `Too many payouts have been made recently, please try again later`,
+          {
+            status: 400,
+          }
+        );
+      }
+    }
     const saveSpin = await supabase.rpc('update_spin_and_points', {
       _game_id: gameId,
       _user_address: userAddress.toLowerCase(),
