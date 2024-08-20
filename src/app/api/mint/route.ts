c99f5984b240eb841197b728e897923551b614d1
Si Wan Kim <siwan.kim@coinbase.com>
2024-08-20 10:52:55 -0700

    pass nonce just for wallet summit q3

--- src/app/api/mint/route.ts
+++ src/app/api/mint/route.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import { type NextRequest, NextResponse } from 'next/server';
+import { ethers } from 'ethers';
 
 import { Database } from '@/utils/database.types';
 import { toBigInt } from '@/utils/toBigInt';
@@ -19,6 +20,12 @@ export async function POST(request: NextRequest) {
   const body: MintData = await request.json();
   const { address, command } = body;
 
+  // TODO: remove this soon
+  const nonce =
+    command === 'wallet-summit-q3'
+      ? ethers.hexlify(ethers.randomBytes(32))
+      : undefined;
+
   if (!address || !command) {
     return new Response(
       `Missing parameters: userAddress: ${address}, command: ${command}`,
@@ -28,7 +35,7 @@ export async function POST(request: NextRequest) {
     );
   }
   try {
-    await airdropNft(address, command);
+    await airdropNft(address, command), nonce;
   } catch (e) {
     console.error(e);
     return NextResponse.json({ success: false });
