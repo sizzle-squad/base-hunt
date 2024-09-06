53b77d19e5c2c21bbcfa66daf6e0446462ff5e19
siwan-cb <136514599+siwan-cb@users.noreply.github.com>
2024-09-06 10:33:55 -0700

    CORS fix on magic-mint/force-airdrop (#473)
    * [WIP]
    
    * CORS fix

--- src/app/api/magic-mint/force-airdrop/route.ts
+++ src/app/api/magic-mint/force-airdrop/route.ts
@@ -21,6 +21,12 @@ export async function POST(req: Request) {
     return NextResponse.json({ status: 'unknown' });
   }
 
+  const origin = req.headers.get('origin');
+
+  if (!origin || !ALLOWED_ORGINS.includes(origin)) {
+    return new Response('Forbidden', { status: 403 });
+  }
+
   console.log(commandLog);
   const { userAddress, command, forced } = await req.json();
 
@@ -36,7 +42,10 @@ export async function POST(req: Request) {
     });
   }
 
-  return NextResponse.json({ status: 'ok' });
+  const res = NextResponse.json({ status: 'ok' });
+  res.headers.set('Access-Control-Allow-Origin', origin); // CORS credentials mode is 'include'
+
+  return res;
 }
 
 function verifyForceAirdropSecret(req: Request): boolean {
@@ -55,6 +64,7 @@ function verifyForceAirdropSecret(req: Request): boolean {
 
 export async function OPTIONS(request: NextRequest) {
   const origin = request.headers.get('origin');
+
   if (!origin || !ALLOWED_ORGINS.includes(origin)) {
     return new Response('Forbidden', { status: 403 });
   }
@@ -62,9 +72,10 @@ export async function OPTIONS(request: NextRequest) {
   return new NextResponse(null, {
     status: 200,
     headers: {
-      'Access-Control-Allow-Origin': origin || '*',
+      'Access-Control-Allow-Origin': origin,
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Headers':
+        'Content-Type, Authorization, x-cb-device-id, x-cb-project-name, x-cb-user-id, x-cb-pagekey, x-cb-is-logged-in, x-cb-platform,  x-cb-project-name, x-cb-session-uuid, x-cb-ujs, x-cb-version-name, x-force-airdrop-secret',
     },
   });
 }
