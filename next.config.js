147127e85badfafa2f40a88c4c8b703e4a5f70dd
Si Wan Kim <siwan.kim@coinbase.com>
2024-09-09 19:03:17 -0700

    remove Access-Control-Allow-Credentials

--- next.config.js
+++ next.config.js
@@ -24,7 +24,6 @@ module.exports = {
         // matching all API routes
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
