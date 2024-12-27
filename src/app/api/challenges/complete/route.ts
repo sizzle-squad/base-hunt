630e57e750b329d613fd1619128db2ae7d5dbd51
akrameldamaty <akram.eldamaty@coinbase.com>
2024-12-27 18:13:00 -0500

    fix

--- src/app/api/challenges/complete/route.ts
+++ src/app/api/challenges/complete/route.ts
@@ -287,7 +287,8 @@ async function pointsMultiplier(
     {
       headers: {
         'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'X-Appsflyer-Id': 'web'
       },
     }
   );
--- src/utils/claims/balanceCheck.ts
+++ src/utils/claims/balanceCheck.ts
@@ -95,7 +95,8 @@ export async function checkTokenIdBalance(
     {
       headers: {
         'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'X-Appsflyer-Id': 'web'
       },
     }
   );
